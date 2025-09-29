357f51115d80d69c2758869a797760d9bf705c4a
Pierre Le Guyader <ple-guya@f1br6s11.clusters.42paris.fr>
2025-09-29 17:56:24 +0200

    prmiere ebauche de index

---
+++ client/public/config/config-tailwind.js
@@ -0,0 +1,16 @@
+
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        'sonpi16-orange': '#ff6000',
+        'sonpi16-black': '#0d0600',
+        'electric': '#0055FF',
+        'lightgray': '#F5F5F5'
+      },
+      fontFamily: {
+        'quency': ['QuencyPixel-Regular', 'sans-serif'],
+      },
+    },
+  },
+};
